b3e9d41a9683c771684c0e46bed810684765da41
hz9527 <981379316@qq.com>
2017-06-21 09:02:33 +0800

    fixjsonp

--- getLocation.js
+++ getLocation.js
@@ -5,11 +5,12 @@ function getJSONP (url, callBack) {
   window[name] = function (res) {
     callBack(res)
     document.querySelector('body').removeChild(script)
+    delete window[name]
   }
   script = document.createElement('script')
   script.type = 'text/javascript'
   document.querySelector('body').appendChild(script)
-  script.src = url + '&output=jsonp&callback=' + name
+  script.src = url + '&callback=' + name
 }
 
 var Position = {
@@ -23,12 +24,14 @@ var Position = {
   typeConf: ['BD', 'SG', 'QQ', 'GD'],
   setConfig: function (options, placeInfo) {
     if (options && options.constructor === Object) {
+      var invalidType = false
       if (config.key) {
         this.config.key = options.key
+        invalidType = -1
       }
       if (options.positionType && options.positionType !== 'default') {
-        if (!options.key) {
-          throw new Error('key is necessary')
+        if (invalidType !== -1 && options.positionType !== 'QQ') {
+          invalidType = true
         }
         if (!this.typeConf.some(key => key === options.positionType)) {
           throw new Error('positionType is invalid')
@@ -36,13 +39,22 @@ var Position = {
         this.config.positionType = options.positionType
       }
       if (placeInfo !== false && options.type) {
-        if (!options.key) {
-          throw new Error('key is necessary')
+        if (invalidType !== -1 && options.type !== 'QQ') {
+          invalidType = true
         }
         if (!this.typeConf.some(key => key === options.type)) {
           throw new Error('type is invalid')
         }
         this.config.type = options.type
+      }
+      if (invalidType === true) {
+        throw new Error('key is necessary')
+      }
+      if (options.canIp === false) {
+        this.config.canIp = false
+      }
+      if (options.timeout > 0) {
+        this.config.timeout = options.timeout
       }
     }
     if (placeInfo === false) {
@@ -67,7 +79,7 @@ var Position = {
   },
   getPostionByIp: {
     'QQ': function (context, success, fail) {
-      getJSONP('//apis.map.qq.com/ws/location/v1/ip?key=' + this.config.key, function (res) {
+      getJSONP('//apis.map.qq.com/ws/location/v1/ip?key=' + this.config.key + '&output=jsonp', function (res) {
         console.log(res)
       })
     }
@@ -77,8 +89,13 @@ var Position = {
       if ('geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition( res => {
           this.posNext(context, success, this.dealPosition.default(res.coords))
-        }, res => {
-          this.getPostionByIp.QQ.call(this, context, success, fail)
+        }, err => {
+          if (this.config.canIp) {
+            this.getPostionByIp.QQ.call(this, context, success, fail)
+          } else {
+            var msg = err.code === 1 ? '获取地理位置权限失败' : err.code === 2 ? '获取地理位置信息失败' : '获取地理位置超时'
+            fail.call(context, msg)
+          }
         }, {
           enableHighAccuracy: true,
           timeout: this.config.timeout,
